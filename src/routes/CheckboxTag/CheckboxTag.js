99df440c9958cb72a6ffcac79765aed7b6bb4dc3
人生海海 <quinlandg@outlook.com>
2018-10-12 11:19:35 +0800

    update checkbox

--- src/routes/CheckboxTag/CheckboxTag.js
+++ src/routes/CheckboxTag/CheckboxTag.js
@@ -33,36 +33,60 @@ export default class index extends PureComponent {
       {
         id: 1,
         props: 'onChange',
-        description: '变化时回调函数',
+        description: '变化时回调函数，返回选中的value',
         type: 'function',
         defaults: '-',
       },
       {
         id: 2,
-        props: 'defaultChecked',
-        description: '初始默认选中与否',
+        props: 'selectAll',
+        description: '全选',
         type: 'boolean',
         defaults: 'false',
       },
       {
         id: 3,
-        props: 'disabled',
-        description: '是否禁用checkbox',
+        props: 'checkedList',
+        description: '初始选中的value',
         type: 'boolean',
         defaults: 'false',
       },
       {
         id: 4,
-        props: 'checked',
-        description: '当前是否被选中',
-        type: 'boolean',
-        defaults: 'false',
+        props: 'list',
+        description: '形如 \n[{ id: 1, label: \'少林足球\', value: \'num1\', checked: false }]',
+        type: 'any[]',
+        defaults: '',
       },
     ];
+
+    const list = [
+      { id: 1, label: '少林足球', value: 'num1', checked: false },
+      { id: 2, label: '行运一条龙', value: 'num2', checked: false },
+      { id: 3, label: '国产凌凌漆', value: 'num3', checked: false },
+      { id: 4, label: '鹿鼎记', value: 'num4', checked: false },
+    ];
+
+    const list1 = [
+      { id: 1, label: '少林足球', value: 'n2um1', checked: false },
+      { id: 2, label: '行运一条龙', value: 'n2um2', checked: false },
+      { id: 3, label: '国产凌凌漆', value: 'n2um3', checked: false },
+      { id: 4, label: '鹿鼎记', value: 'nu2m4', checked: false },
+    ];
+
+    const list2 = [
+      { id: 1, label: '少林足球', value: 'n2um13', checked: false },
+      { id: 2, label: '行运一条龙', value: 'n2um23', checked: false },
+      { id: 3, label: '国产凌凌漆', value: 'n2um33', checked: false },
+      { id: 4, label: '鹿鼎记', value: 'nu2m43', checked: false },
+    ];
+    //
     return (
       <div>
         <Header>CheckboxTag 多选标签</Header>
-        <CheckboxTag />
+        <CheckboxTag list={list} onChange={(e) => {console.log(e)}} />
+        <CheckboxTag list={list1} selectAll />
+        <CheckboxTag list={list2} checkedList={['n2um13', 'n2um33']} />
         <div>
           <Table columns={columns} data={data} rowKey="id" title="API" />
         </div>
--- src/routes/CheckboxTag/index.js
+++ src/routes/CheckboxTag/index.js
@@ -1,20 +1,60 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import styles from './style/index.less';
 
-export default class index extends PureComponent {
-  state = {};
+export default class index extends Component {
+  state = {
+    list: [],
+  };
+
+  componentWillMount() {
+    const { selectAll = false, checkedList = [], list = [] } = this.props
+    if (selectAll) {
+      list.forEach((item) => {
+          item.checked = true // eslint-disable-line
+      })
+    }
+    !selectAll && checkedList.forEach((item) => { // eslint-disable-line
+      list.forEach((item1) => {
+        if (item === item1.value) {
+          item1.checked = true // eslint-disable-line
+        }
+      })
+    })
+    this.setState({
+      list,
+    });
+  }
+
+  onChange = value => {
+    const { list } = this.state;
+    const { onChange } = this.props
+    list.forEach(item => {
+      item.checked = value === item.value ? !item.checked : item.checked;
+    });
+    this.setState({
+      list,
+    })
+    const checkedList = []
+    list.forEach((item) => {
+      if (item.checked) {
+        checkedList.push(item.value)
+      }
+    })
+    onChange(checkedList)
+  };
 
   render() {
-    const list = [
-      { id: 1, label: '少林足球', value: 'num1' },
-      { id: 2, label: '行运一条龙', value: 'num2' },
-      { id: 3, label: '国产凌凌漆', value: 'num3' },
-      { id: 4, label: '鹿鼎记', value: 'num4' },
-    ];
+    const { list } = this.state;
     return (
       <div>
         {list.map(item => (
-          <span key={item.id} className={styles.checkboxTag}>{item.label}</span>
+          <span
+            key={item.id}
+            className={styles[item.checked ? 'checked' : 'checkboxTag']}
+            onClick={() => this.onChange(item.value)}
+          >
+            {item.label}
+          </span>
         ))}
       </div>
     );
